30eaccce8baff857fd671b39434baacebe069589
Nathan <nembery@gmail.com>
2021-01-20 11:48:27 -0500

    only output ip addresses

--- terraform/azure/outputs.tf
+++ terraform/azure/outputs.tf
@@ -1,12 +1,12 @@
 output "ngfw_ip_address" {
-  value = azurerm_public_ip.fw_mgmt_ip
+  value = azurerm_public_ip.fw_mgmt_ip.ip_address
 }
 
 output "client_01_ip_address" {
-  value = azurerm_public_ip.client_01_mgmt_ip
+  value = azurerm_public_ip.client_01_mgmt_ip.ip_address
 }
 
 output "client_02_ip_addrress" {
-  value = azurerm_public_ip.client_02_mgmt_ip
+  value = azurerm_public_ip.client_02_mgmt_ip.ip_address
 }
 
