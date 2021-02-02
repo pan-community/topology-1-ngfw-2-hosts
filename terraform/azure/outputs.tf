ff59b15bfb8647260a66175382154691b5ecfa3a
Scott Shoaf <scotchoaf@gmail.com>
2021-02-02 12:47:25 -0500

    add hidden vars, fix outputs typos, add output .j2 ip addr table

--- terraform/azure/outputs.tf
+++ terraform/azure/outputs.tf
@@ -6,7 +6,7 @@ output "client_01_ip_address" {
   value = azurerm_public_ip.client_01_mgmt_ip.ip_address
 }
 
-output "client_02_ip_addrress" {
+output "client_02_ip_address" {
   value = azurerm_public_ip.client_02_mgmt_ip.ip_address
 }
 
