e2066d19686a65464d444c7d3fe7a5a9f6cdd48f
Scott Shoaf <scotchoaf@gmail.com>
2021-02-17 17:02:51 -0500

    add more outputs for private addresses

--- terraform/azure/outputs.tf
+++ terraform/azure/outputs.tf
@@ -7,42 +7,42 @@ output "jumphost_ip_address" {
 }
 
 output "client_01_trust_ip" {
-  value = azurerm_network_interface.client-01-eth0.private_ip_address
+  value = azurerm_network_interface.client-01-eth0.ip_configuration.private_ip_address
 }
 
 output "client_02_dmz_ip" {
-  value = azurerm_network_interface.client-02-eth0.private_ip_address
+  value = azurerm_network_interface.client-02-eth0.ip_configuration.private_ip_address
 }
 
 output "fw_mgmt_ip" {
-  value = azurerm_network_interface.fw_mgmt.private_ip_address
+  value = azurerm_network_interface.fw_mgmt.ip_configuration.private_ip_address
 }
 
 output "fw_dmz_ip" {
-  value = azurerm_network_interface.fw_dmz.private_ip_address
+  value = azurerm_network_interface.fw_dmz.ip_configuration.private_ip_address
 }
 
 output "fw_trust_ip" {
-  value = azurerm_network_interface.fw_trust.private_ip_address
+  value = azurerm_network_interface.fw_trust.ip_configuration.private_ip_address
 }
 
 output "fw_untrust_ip" {
-  value = azurerm_network_interface.fw_untrust.private_ip_address
+  value = azurerm_network_interface.fw_untrust.ip_configuration.private_ip_address
 }
 
 output "jumphost_mgmt_ip" {
-  value = azurerm_network_interface.jumphost-eth0.private_ip_address
+  value = azurerm_network_interface.jumphost-eth0.ip_configuration.private_ip_address
 }
 
 output "untrust_subnet_cidr" {
-  value = azurerm_subnet.untrust.attributes.address_prefix
+  value = azurerm_subnet.untrust.address_prefixes
 }
 
 output "trust_subnet_cidr" {
-  value = azurerm_subnet.trust.attributes.address_prefix
+  value = azurerm_subnet.trust.address_prefixes
 }
 
 output "dmz_subnet_cidr" {
-  value = azurerm_subnet.dmz.attributes.address_prefix
+  value = azurerm_subnet.dmz.address_prefixes
 }
 
