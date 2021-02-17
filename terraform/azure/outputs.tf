c6e283ab417b5a4f031e0f7aa57b7491adc6b4e8
Scott Shoaf <scotchoaf@gmail.com>
2021-02-17 16:57:48 -0500

    add more outputs for private addresses

--- terraform/azure/outputs.tf
+++ terraform/azure/outputs.tf
@@ -6,3 +6,43 @@ output "jumphost_ip_address" {
   value = azurerm_public_ip.jumphost_mgmt_ip.ip_address
 }
 
+output "client_01_trust_ip" {
+  value = azurerm_network_interface.client-01-eth0.private_ip_address
+}
+
+output "client_02_dmz_ip" {
+  value = azurerm_network_interface.client-02-eth0.private_ip_address
+}
+
+output "fw_mgmt_ip" {
+  value = azurerm_network_interface.fw_mgmt.private_ip_address
+}
+
+output "fw_dmz_ip" {
+  value = azurerm_network_interface.fw_dmz.private_ip_address
+}
+
+output "fw_trust_ip" {
+  value = azurerm_network_interface.fw_trust.private_ip_address
+}
+
+output "fw_untrust_ip" {
+  value = azurerm_network_interface.fw_untrust.private_ip_address
+}
+
+output "jumphost_mgmt_ip" {
+  value = azurerm_network_interface.jumphost-eth0.private_ip_address
+}
+
+output "untrust_subnet_cidr" {
+  value = azurerm_subnet.untrust.attributes.address_prefix
+}
+
+output "trust_subnet_cidr" {
+  value = azurerm_subnet.trust.attributes.address_prefix
+}
+
+output "dmz_subnet_cidr" {
+  value = azurerm_subnet.dmz.attributes.address_prefix
+}
+
